b1190ca1f68456d2a7d614cafab83a26cb848633
M.Mehdi <m.sfj9696@gmail.com>
2021-12-06 16:07:46 +0330

    Init commit.

---
+++ src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/StartSpringImplMehdiSarfApplication.java
@@ -0,0 +1,13 @@
+package com.mehdisarf.StartSpringImplMehdiSarf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringImplMehdiSarfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSpringImplMehdiSarfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehdisarf/StartSpringImplMehdiSarf/StartSpringImplMehdiSarfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehdisarf.StartSpringImplMehdiSarf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpringImplMehdiSarfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
