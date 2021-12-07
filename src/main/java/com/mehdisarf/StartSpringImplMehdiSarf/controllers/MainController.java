497648f73bfd49ba0ac0bfd640628578bc3f12ef
M.Mehdi <m.sfj9696@gmail.com>
2021-12-07 21:35:49 +0330

    Send data from client to server using request parameter

--- src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/controllers/MainController.java
+++ src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/controllers/MainController.java
@@ -3,15 +3,18 @@ package com.mehdisarf.StartSpringImplMehdiSarf.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(Model model) {
+    public String home(@RequestParam String color, Model model) {
+        // tells Spring it needs to get the value from the HTTP request parameter
+        // with the same name as the method’s parameter name.
 
+        model.addAttribute("color", color);
         model.addAttribute("username", "Qolam");
-        model.addAttribute("color", "red");
 
         return "home.html";
     }
