408b2bafd6668acce773a63f5828cc009fe4b7c4
M.Mehdi <m.sfj9696@gmail.com>
2021-12-14 02:48:05 +0330

    Count login number using application-scoped bean

--- src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/controllers/MainController.java
+++ src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.mehdisarf.StartSpringImplMehdiSarf.controllers;
 
 import com.mehdisarf.StartSpringImplMehdiSarf.LoggedUserManagement;
+import com.mehdisarf.StartSpringImplMehdiSarf.LoginCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,27 +12,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 
     private final LoggedUserManagement loggedUserManagement;
+    private final LoginCounter loginCounter;
 
     @Autowired
-    public MainController(LoggedUserManagement loggedUserManagement) {
+    public MainController(LoggedUserManagement loggedUserManagement, LoginCounter loginCounter) {
         this.loggedUserManagement = loggedUserManagement;
+        this.loginCounter = loginCounter;
     }
 
     @RequestMapping(value = "/main")
-    public String home(@RequestParam(required = false) String logout,
-                       Model model) {
+    public String home(
+            @RequestParam(required = false) String logout,
+            Model model) {
 
         if (logout != null) {
             loggedUserManagement.setUsername(null);
         }
 
         String username = loggedUserManagement.getUsername();
+        long count = loginCounter.getCounter();
 
         // username should be different than null if someone logged in.
         if (username == null)
             return "redirect:/";
 
-        model.addAttribute("username" , username);
+        model.addAttribute("username", username);
+        model.addAttribute("loginCount", count);
 
         return "main.html";
     }
--- src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/model/LoginProcessor.java
+++ src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/model/LoginProcessor.java
@@ -1,6 +1,7 @@
 package com.mehdisarf.StartSpringImplMehdiSarf.model;
 
 import com.mehdisarf.StartSpringImplMehdiSarf.LoggedUserManagement;
+import com.mehdisarf.StartSpringImplMehdiSarf.LoginCounter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -11,15 +12,19 @@ public class LoginProcessor {
 
     private String username;
     private String password;
-    private LoggedUserManagement loggedUserManagement;
+    private final LoggedUserManagement loggedUserManagement;
+    private final LoginCounter loginCounter;
 
     @Autowired
-    public LoginProcessor(LoggedUserManagement loggedUserManagement) {
+    public LoginProcessor(LoggedUserManagement loggedUserManagement, LoginCounter loginCounter) {
         this.loggedUserManagement = loggedUserManagement;
+        this.loginCounter = loginCounter;
     }
 
 
     public boolean login() {
+
+        loginCounter.increment();
 
         boolean loginResult = false;
 
