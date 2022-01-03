4350461e77bab3a3226fb98c245b1b05aa91a851
M.Mehdi <m.sfj9696@gmail.com>
2022-01-03 16:39:33 +0330

    Send information about the exception that occurred, from the controller’s action to the advice by adding a parameter to the advice’s exception handler method of the type of the handled exception.

--- src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/controllers/advice/ExceptionControllerAdvice.java
+++ src/main/java/com/mehdisarf/StartSpringImplMehdiSarf/controllers/advice/ExceptionControllerAdvice.java
@@ -9,12 +9,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
+    // send information about the exception that occurred, from the controller’s action to the
+    // advice by adding a parameter to the advice’s exception handler method of the type of the handled exception.
     @ExceptionHandler(NotEnoughMoneyException.class)
-    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
+    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler(NotEnoughMoneyException exception) {
+        // Spring is smart enough to pass the exception reference from the controller
+        // to the advice’s exception handler method. You can then use any details of the
+        // exception instance in the advice’s logic
 
         ErrorDetails errorDetails = new ErrorDetails();
 
-        errorDetails.setMessage("Not enough money to make the payment.");
+        errorDetails.setMessage(exception.getLocalizedMessage() + " " + exception.getClass().getName() + " Not enough money to make the payment.");
 
         return ResponseEntity.badRequest().body(errorDetails);
     }
